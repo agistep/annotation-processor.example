875b294c5c79ea2fb27bd7c6e0eb69263a42422f
lenkim <joenggyu0@gmail.com>
2023-06-26 00:09:28 +0900

    implement lombok.st annotation processor

--- fake-lombok/src/main/java/com/likelen/annotation/GetProcessor.java
+++ fake-lombok/src/main/java/com/likelen/annotation/GetProcessor.java
@@ -1,6 +1,5 @@
 package com.likelen.annotation;
 
-import com.google.auto.service.AutoService;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.util.TreePath;
@@ -26,11 +25,9 @@ import java.util.Set;
 /**
  * SupportedAnnotationTypes 어떤 어노테이션을 위한 프로세서 인가?
  * SupportedSourceVersion jdk 지원 정보
- * AutoService(Processor.class) MAINFEST 자동생성
  */
 @SupportedAnnotationTypes("com.likelen.annotation.Getter")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-@AutoService(Processor.class)
 public class GetProcessor extends AbstractProcessor {
     private ProcessingEnvironment processingEnvironment;
     private Trees trees;
@@ -39,9 +36,9 @@ public class GetProcessor extends AbstractProcessor {
     private Context context;
 
     /**
-     *             1. names 추후 메소드를 생성에서, parm or method 이름 생성을 위함.
-     *             2. Treemaker Abstract Syntax Tree 를 make 하는 메소드 제공
-     *             예) method 정의, parameter 값 정의 etc..
+     * 1. names 추후 메소드를 생성에서, parm or method 이름 생성을 위함.
+     * 2. Treemaker Abstract Syntax Tree 를 make 하는 메소드 제공
+     * 예) method 정의, parameter 값 정의 etc..
      */
 
     @Override
@@ -57,14 +54,15 @@ public class GetProcessor extends AbstractProcessor {
     }
 
     /**
-     *  process 의 리턴값으로 어놈테이션을 처리하고 난 뒤, 다른 어노테이션이 지원되지 않도록 조정
+     * process 의 리턴값으로 어놈테이션을 처리하고 난 뒤, 다른 어노테이션이 지원되지 않도록 조정
+     *
      * @return true (이 필드, 클래스는 끝남) or false (이, 필드 클래스는 끝나지 않음)
      */
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         System.out.println("process 메서드 실행");
         // TreePathScanner 모든 하위 트리노드를 방문하고, 상위 노드에 대한 경로를 유지하는 tree visitor
-        TreePathScanner<Object, CompilationUnitTree> scanner = new TreePathScanner<Object, CompilationUnitTree>(){
+        TreePathScanner<Object, CompilationUnitTree> scanner = new TreePathScanner<Object, CompilationUnitTree>() {
             /**
              * CompillationUnitTree 는 소스파일에서 패키지 선언에서 부터 abstract syntax tree 를 정의함
              * ClassTree -> 클래스 , 인터페이스, enum 어노테이션을 트리노드로 선언
@@ -72,10 +70,10 @@ public class GetProcessor extends AbstractProcessor {
              * CompilationUnitTree AST(Abstract Syntax Tree 의 최상단)
              */
             @Override
-            public Trees visitClass(ClassTree classTree, CompilationUnitTree unitTree){
+            public Trees visitClass(ClassTree classTree, CompilationUnitTree unitTree) {
                 JCTree.JCCompilationUnit compilationUnit = (JCTree.JCCompilationUnit) unitTree;
                 // .java 파일인지 확인후 accept 를 통해 treeTranslater, 작성 메소드 생성
-                if (compilationUnit.sourcefile.getKind() == JavaFileObject.Kind.SOURCE){
+                if (compilationUnit.sourcefile.getKind() == JavaFileObject.Kind.SOURCE) {
                     compilationUnit.accept(new TreeTranslator() {
                         @Override
                         public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
@@ -83,11 +81,11 @@ public class GetProcessor extends AbstractProcessor {
                             // Class 내부에 정의된 모든 member 를 싹다 가져옴.
                             List<JCTree> members = jcClassDecl.getMembers();
                             // Syntax tree 에서 모든 member 변수 get
-                            for(JCTree member : members){
-                                if (member instanceof JCTree.JCVariableDecl){
+                            for (JCTree member : members) {
+                                if (member instanceof JCTree.JCVariableDecl) {
                                     // member 변수에 대한 getter 메서드 생성
                                     List<JCTree.JCMethodDecl> getters = createGetter((JCTree.JCVariableDecl) member);
-                                    for(JCTree.JCMethodDecl getter : getters){
+                                    for (JCTree.JCMethodDecl getter : getters) {
                                         jcClassDecl.defs = jcClassDecl.defs.prepend(getter);
                                     }
                                 }
@@ -98,15 +96,15 @@ public class GetProcessor extends AbstractProcessor {
                 return trees;
             }
         };
-        /**
+        /***
          * RoundEnvironment
          * getElementsAnnotatedWith() -> @Get 의 어노테이션이 붙여져 있는 모든 element 를 불러 일으킨다.
          */
         for (final Element element : roundEnv.getElementsAnnotatedWith(Getter.class)) {
             // 현재 어노테이션은 Type 이고 여기서 Class 뿐만 아니라, interface 와 enum 에도 작성이 가능하므로 class만 지정할 수 있도록
-            if(element.getKind() != ElementKind.CLASS){
+            if (element.getKind() != ElementKind.CLASS) {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "@Get annotation cant be used on" + element.getSimpleName());
-            }else{
+            } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "@Get annotation Processing " + element.getSimpleName());
                 final TreePath path = trees.getPath(element);
                 scanner.scan(path, path.getCompilationUnit());
@@ -116,10 +114,10 @@ public class GetProcessor extends AbstractProcessor {
         return true;
     }
 
-    public List<JCTree.JCMethodDecl> createGetter(JCTree.JCVariableDecl var){
+    public List<JCTree.JCMethodDecl> createGetter(JCTree.JCVariableDecl var) {
         // 필드 이름 변수에 앞문자 대문자로 변경 해주기
         String str = var.name.toString();
-        String upperVar = str.substring(0,1).toUpperCase()+str.substring(1,var.name.length());
+        String upperVar = str.substring(0, 1).toUpperCase() + str.substring(1, var.name.length());
 
         return List.of(
                 /**
---
+++ foo-client/src/main/java/com/likelen/Bar.java
@@ -0,0 +1,9 @@
+package com.likelen;
+
+import com.likelen.annotation.Getter;
+
+@Getter
+class Bar {
+
+    private String name = "foo";
+}
